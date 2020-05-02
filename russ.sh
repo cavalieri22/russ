e95020952b0a2868656b3abca7b8d4dff96c9e44
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-05-02 15:16:28 -0500

    Update russ.sh

--- russ.sh
+++ russ.sh
@@ -3,4 +3,4 @@ apt-get update -y; apt-get upgrade -y;
 update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvcnVzcy9tYXN0ZXIvaW5zdGFsYS5zaA=='
 dom='base64 -d'
 RE=$(echo $update1|$dom)
-wget -O instala.sh $RE;chmod +x instala.sh;./instala.sh
+wget -O instala.sh $RE;chmod 777 instala.sh* && ./instala.sh*
