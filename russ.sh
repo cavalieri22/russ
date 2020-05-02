793030745241f4ff0804cfc60d31730b7bdc07ef
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-05-02 13:14:48 -0500

    Create russ.sh

---
+++ russ.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvcnVzcy9tYXN0ZXIvaW5zdGFsYS5zaA=='
+dom='base64 -d'
+RE=$(echo $update1|$dom)
+wget -q $RE; chmod +x /instala.sh;./instala.sh
