829864085b043058958b0c7ce620238f9e12ec70
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-05-02 14:29:30 -0500

    Update russ.sh

--- russ.sh
+++ russ.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt-get update -y; apt-get upgrade -y;
 update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvcnVzcy9tYXN0ZXIvaW5zdGFsYS5zaA=='
 dom='base64 -d'
 RE=$(echo $update1|$dom)
