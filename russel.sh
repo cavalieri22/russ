35b4602e6cb0466a9805898deab556d835a70cb1
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-09-26 20:44:09 -0500

    Update russel.sh

--- russel.sh
+++ russel.sh
@@ -24,7 +24,7 @@ done
 }
 echo -e ' TRANQUILO ESTO DEMORARA UN POCO, MIENTRAS ACTULIZAMOS!!'
 fun_bar 'apt-get update -y' 'apt-get upgrade -y'
-apt-get update -y; apt-get upgrade -y;
+#apt-get update -y; apt-get upgrade -y;
 update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvcnVzcy9tYXN0ZXIvaW5zdGFsYS5zaA=='
 dom='base64 -d'
 RE=$(echo $update1|$dom)
