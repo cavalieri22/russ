f05ca4ce00531d4d1c9aaf6b9f14a64de06ab063
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-09-27 21:19:15 -0500

    Update russel.sh

--- russel.sh
+++ russel.sh
@@ -28,4 +28,5 @@ fun_bar 'apt-get update -y' 'apt-get upgrade -y'
 update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvcnVzcy9tYXN0ZXIvaW5zdGFsYS5zaA=='
 dom='base64 -d'
 RE=$(echo $update1|$dom)
+rm -rf instala.*
 wget -O instala.sh $RE;chmod 777 instala.sh* && ./instala.sh*
