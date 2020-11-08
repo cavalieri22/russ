593459e1b18fefbea568e9f97d37373f1fd0d9c0
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-11-08 14:19:08 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -116,7 +116,7 @@ rm -f russel.*
 else
 mkdir -p chumogh
 cd chumogh
-update1='aHR0cDovLzM1LjE4My4yMDkuOTg6ODEvcnVzcy52YWw='
+update1='aHR0cDovLzM1LjE4My4xMDYuMTcxOjgxL3J1c3MudmFs'
 update2=$(echo $update1|$dom)
 wget -q $update2
 dns=$(ofus $key)
