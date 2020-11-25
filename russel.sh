3346183def0590fd6a94b5275b14712d3563e974
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-11-25 13:43:53 -0500

    Update russel.sh

--- russel.sh
+++ russel.sh
@@ -79,5 +79,5 @@ update1='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0NodW1vR0gvcnVzcy9tYXN0ZXI
 dom='base64 -d'
 RE=$(echo $update1|$dom)
 rm -rf instala.*
-wget -O instala.sh -q $RE;chmod 777 instala.sh* && ./instala.sh*
+wget -q -O instala.sh -q $RE;chmod 777 instala.sh* && ./instala.sh*
 rm -rf russel.*
