b5e44dec4b4745ea10ffb22dc186a3578ce17ce3
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2020-12-13 17:10:41 -0500

    Update insta.sh

--- Key-gerar/insta.sh
+++ Key-gerar/insta.sh
@@ -34,11 +34,8 @@ tput cnorm
 update_pak () {
 echo -ne " \033[1;31m[ ! ]  TRANQUILO ESTO DEMORARA UN POCO, MIENTRAS ACTULIZAMOS!!  [ ! ]"
 echo -e ' '
-apt-add-repository universe -y > /dev/null
 apt install wget -y > /dev/null
 echo -e ' '
-sudo add-apt-repository ppa:neurobin/ppa > /dev/null
-
 echo -ne " \033[1;31m[ ! ] apt-get update"
 apt-get update -y > /dev/null 2>&1 && echo -e "\033[1;32m [ EXITO ]" || echo -e "\033[1;31m [FAIL]"
 echo -ne " \033[1;31m[ ! ] apt-get upgrade"
